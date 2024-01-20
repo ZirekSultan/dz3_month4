40c2e7d086baa9542ba61bdcc14da8dcc1492d53
ZirekSultan <sultanovaaiz17@gmail.com>
2024-01-20 12:38:16 +0600

    Dz3

--- src/App.js
+++ src/App.js
@@ -1,25 +1,69 @@
-import logo from './logo.svg';
-import './App.css';
+import React , {useState} from 'react'
+
+const containerStyle = {
+    textAlign: 'center',
+    margin: '50px',
+};
+
+const inputStyle = {
+    marginRight: '20px',
+};
+
+const buttonStyle = {
+    backgroundColor: 'yellow',
+    color: 'black',
+    padding: '15px 20px',
+    textAlign: 'center',
+    display: 'inline-block',
+    fontSize: '20px',
+    margin: '6px 8px',
+};
+
+const toDoListStyle = {
+    listStyleType: 'none',
+    padding: '0',
+};
+
+const toDoItemStyle = {
+    margin: '20px',
+    padding: '10px',
+    border: '1px solid #ccc',
+};
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [toDo, setToDo] = useState([])
+    const [newToDos, setNewToDos] = useState('')
+
+    const addTodo = () => {
+        if (newToDos !== '') {
+            setNewToDos([...toDo , newToDos])
+            setNewToDos('')
+        }
+    }
+    return (
+        <div style={containerStyle}>
+            <h1>Todo App</h1>
+            <div>
+                <input
+                    type="text"
+                    value={newToDos}
+                    onChange={(e) => setNewToDos(e.target.value)}
+                    style={inputStyle}
+                />
+                <button onClick={addTodo} style={buttonStyle}>
+                    Добавить задачу
+                </button>
+            </div>
+            <ul style={toDoListStyle}>
+                {toDo.map((toDo, index) => (
+                    <li key={index} style={toDoItemStyle}>
+                        {toDo}
+                    </li>
+                ))}
+            </ul>
+        </div>
+    )
 }
 
-export default App;
+export default App
